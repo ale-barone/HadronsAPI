23df61c727edfdd71cf7c5ae06d4845e0fa3be8c
Alessandro Barone <ab1n19@soton.ac.uk>
2022-04-02 20:30:35 +0100

    Add 2pt and 3pt contraction

--- AContraction.hpp
+++ AContraction.hpp
@@ -2,61 +2,208 @@
 
 BEGIN_APIMODULE_NAMESPACE(AContraction)
 
+////////////////////////////////////////////////////////////////////////////////
+// assign parameters of the contraction
+////////////////////////////////////////////////////////////////////////////////
+
 template <typename TContraction>
-void assign_contraction_par(TContraction &contraction, std::string q1, std::string q2, std::array<std::string, 2> gammas, std::string sink){
+void assign_contraction_par(TContraction &contraction,
+                            std::string q1,
+                            std::string q2,
+                            std::array<std::string, 2> gammas,
+                            std::string sink){
     contraction.q1 = q1;
     contraction.q2 = q2;
+    contraction.sink = sink;
+    // gammas
     std::string gamma_snk = gammas[0];
     std::string gamma_src = gammas[1];
     contraction.gammas = "(" + gamma_snk + " " + gamma_src + ")";
-    contraction.sink = sink;
 }
 
-// general 2pt contraction
-std::string make_2pt_contraction(Application &application, std::string output_folder, std::string q1, std::string q2, std::array<std::string, 2> gammas, std::string sink){
-    MContraction::Meson::Par contraction;
-    assign_contraction_par(contraction, q1, q2, gammas, sink);
-    
-    std::string snkmom = get_snkmom(sink);
-    std::string contraction_name = "2pt_snkmom_" + snkmom + "_"
-                                    + contraction.q1 + "_" 
-                                    + gamma_snk + "_" 
-                                    + contraction.q2;
-    contraction.output = output_folder + contraction_name;
+template <typename TContraction>
+void assign_contraction_par(TContraction &contraction,
+                            std::string q1,
+                            std::string q2,
+                            std::vector<std::array<std::string, 2>> gammas_list,
+                            std::string sink){
+    contraction.q1 = q1;
+    contraction.q2 = q2;
+    contraction.sink = sink;
+    // gammas
+    std::string gammas = "";
+    for (int g=0; g<gammas_list.size(); g++){
+        std::string gamma_snk = gammas_list[g][0];
+        std::string gamma_src = gammas_list[g][1];
+        gammas += "(" + gamma_snk + " " + gamma_src + ")";
+    }
+    contraction.gammas = gammas;
+}
 
-    application.createModule<MContraction::Meson>(contraction_name, contraction);
+template <typename TContraction>
+void assign_contraction_par(TContraction &contraction,
+                            std::string q1,
+                            std::string q2,
+                            std::vector<std::string> gammas_snk_list,
+                            std::string gamma_src,
+                            std::string sink){
+    contraction.q1 = q1;
+    contraction.q2 = q2;
+    contraction.sink = sink;
+    // gammas
+    std::string gammas = "";
+    for (int g=0; g<gammas_snk_list.size(); g++){
+        std::string gamma_snk = gammas_snk_list[g];
+        gammas += "(" + gamma_snk + " " + gamma_src + ")";
+    }
+    contraction.gammas = gammas;    
+}
+
+// make name
+template <typename TContraction>
+std::string make_contraction_name(TContraction &contraction,
+                                  std::string prefix,
+                                  std::string gamma_snk){
+    std::string snkmom = get_snkmom(contraction.sink);
+    std::string contraction_name = prefix + "_"
+                                   + "snkmom_" + snkmom + "_"
+                                   + contraction.q1 + "_"
+                                   + gamma_snk + "_" 
+                                   + contraction.q2;
     return contraction_name;
 }
 
-// // general contraction
-// std::string make_contraction(Application &application,std::string prefix, std::string q1, std::string q2, std::array<std::string, 2> gammas, std::string sink){
+////////////////////////////////////////////////////////////////////////////////
+// generic contraction (internal)
+////////////////////////////////////////////////////////////////////////////////
+
+// general contraction
+void make_contraction(Application &application,
+                       std::string prefix,
+                       std::string q1,
+                       std::string q2,
+                       std::array<std::string, 2> gammas,
+                       std::string sink,
+                       std::string folder_output){
+
+    MContraction::Meson::Par contraction;
+    assign_contraction_par(contraction, q1, q2, gammas, sink);
+
+    std::string gamma_snk = gammas[0];
+    std::string contraction_name = make_contraction_name(contraction, prefix, gamma_snk);
+    contraction.output = folder_output + "/" + contraction_name;
+    application.createModule<MContraction::Meson>(contraction_name, contraction);
+}
+
+// general contraction for list of couples {gammas_snk, gamma_src}
+void make_contraction(Application &application,
+                       std::string prefix,
+                       std::string q1,
+                       std::string q2,
+                       std::vector<std::array<std::string, 2>> gammas_list,
+                       std::string sink,
+                       std::string folder_output){
     
-//     MContraction::Meson::Par contraction;
-//     std::string gamma_snk = gammas[0];
-//     std::string gamma_src = gammas[1];
-   
-//     contraction.q1 = q1;
-//     contraction.q2 = q2;
-//     contraction.gammas = "(" + gamma_snk + " " + gamma_src + ")";
-//     contraction.sink = sink;
+    MContraction::Meson::Par contraction;
+    assign_contraction_par(contraction, q1, q2, gammas_list, sink);
+    std::string contraction_name = make_contraction_name(contraction, prefix, "Gammas");
+    contraction.output = folder_output + "/" + contraction_name;
+    application.createModule<MContraction::Meson>(contraction_name, contraction);
+}
 
-//     std::string mom;
-//     std::string twist;
-//     if (contraction.q1.find("tw") !=std::string::npos){
-//         twist = get_twist(contraction.q1) + "_"; }
-//     else if (contraction.q2.find("tw") !=std::string::npos){
-//         twist = get_twist(contraction.q2) + "_";
-//     }
+// general contraction for list of gammas_snk
+void make_contraction(Application &application,
+                          std::string prefix,
+                          std::string q1,
+                          std::string q2,
+                          std::vector<std::string> gammas_snk_list,
+                          std::string gamma_src,
+                          std::string sink,
+                          std::string folder_output){
 
-//     std::string contraction_name = prefix + "_" + twist 
-//                                        + remove_twist_str(contraction.q1) + "_" 
-//                                        + gamma_snk + "_" 
-//                                        + remove_twist_str(contraction.q2);
-//     contraction.output = folder_output + contraction_name;
+    // create contraction
+    MContraction::Meson::Par contraction;
+    assign_contraction_par(contraction, q1, q2, gammas_snk_list, gamma_src, sink);
+    std::string contraction_name = make_contraction_name(contraction, prefix, "Gammas");
+    contraction.output = folder_output + "/" + contraction_name;
+    application.createModule<MContraction::Meson>(contraction_name, contraction);
+}
 
-//     application.createModule<MContraction::Meson>(contraction_name, contraction);
-//     return contraction_name;
-// }
+////////////////////////////////////////////////////////////////////////////////
+// 2pt contraction
+////////////////////////////////////////////////////////////////////////////////
 
+// general 2pt contraction
+void make_2pt_contraction(Application &application,
+                          std::string q1,
+                          std::string q2,
+                          std::array<std::string, 2> gammas,
+                          std::string sink,
+                          std::string folder_output="output/2pt"){
+
+    make_contraction(application, "2pt", q1, q2, gammas, sink, folder_output);
+}
+
+// general 2pt contraction for list of couples {gammas_snk, gamma_src}
+void make_2pt_contraction(Application &application,
+                          std::string q1,
+                          std::string q2,
+                          std::vector<std::array<std::string, 2>> gammas_list,
+                          std::string sink,
+                          std::string folder_output="output/2pt"){
+    
+    make_contraction(application, "2pt", q1, q2, gammas_list, sink, folder_output);
+}
+
+// general 2pt contraction for list of gammas_snk
+void make_2pt_contraction(Application &application,
+                          std::string q1,
+                          std::string q2,
+                          std::vector<std::string> gammas_snk_list,
+                          std::string gamma_src,
+                          std::string sink,
+                          std::string folder_output="output/3pt"){
+
+    make_contraction(application, "3pt", q1, q2, gammas_snk_list, gamma_src, sink, folder_output);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// 3pt contraction
+////////////////////////////////////////////////////////////////////////////////
+
+// general 3pt contraction
+void make_3pt_contraction(Application &application,
+                          std::string q1,
+                          std::string q2,
+                          std::array<std::string, 2> gammas,
+                          std::string sink,
+                          std::string folder_output="output/3pt"){
+
+    make_contraction(application, "3pt", q1, q2, gammas, sink, folder_output);
+}
+
+// general 3pt contraction for list of couples {gammas_snk, gamma_src}
+void make_3pt_contraction(Application &application,
+                          std::string q1,
+                          std::string q2,
+                          std::vector<std::array<std::string, 2>> gammas_list,
+                          std::string sink,
+                          std::string folder_output="output/3pt"){
+    
+    make_contraction(application, "3pt", q1, q2, gammas_list, sink, folder_output);
+}
+
+// general 3pt contraction for list of gammas_snk
+void make_3pt_contraction(Application &application,
+                          std::string q1,
+                          std::string q2,
+                          std::vector<std::string> gammas_snk_list,
+                          std::string gamma_src,
+                          std::string sink,
+                          std::string folder_output="output/3pt"){
+
+    make_contraction(application, "3pt", q1, q2, gammas_snk_list, gamma_src, sink, folder_output);
+}
 
 END_APIMODULE_NAMESPACE
