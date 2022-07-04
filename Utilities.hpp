b02a7db9f6ead9d9ae960a83df88d3f16c51a8fe
Alessandro Barone <ab1n19@soton.ac.uk>
2022-07-04 17:47:21 +0100

    Update Utilities

--- Utilities.hpp
+++ Utilities.hpp
@@ -10,18 +10,18 @@ using namespace Hadrons;
 #ifndef Hadrons_API_Utilities_hpp_
 #define Hadrons_API_Utilities_hpp_
 
+// API namespace
 #define BEGIN_API_NAMESPACE \
 namespace API {
-
 #define END_API_NAMESPACE }
 
+// APIMODULE namespace + ifndef
 #define BEGIN_APIMODULE_NAMESPACE(name)\
 BEGIN_API_NAMESPACE \
 namespace name {
 
 #define END_APIMODULE_NAMESPACE \
-END_API_NAMESPACE \
-}
+END_API_NAMESPACE }\
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -55,6 +55,13 @@ int count_negative(std::string string){
     return neg;
 }
 
+// check if string "toFind" is in "string"
+bool has_string(std::string string, std::string toFind){
+    size_t pos = string.find(toFind);
+    bool has = (pos != std::string::npos) ? true : false;
+    return has;
+}
+
 // remove "_mom_" string and the value of momentum in the form "x_x_x_x"
 std::string remove_mom_str(std::string string){ 
     std::string toErase = "_mom_";
@@ -82,13 +89,15 @@ std::string copy_from(std::string string, std::string begin, size_t len = std::s
 // find the 4th underscore. The string is supposed to start with the
 // momentum info "(mom)_x_x_x_x_*" and the 4th underscore is the last one after
 // the last component. If it's not there it must mean that the string is terminated
-size_t pos_5th_underscore(std::string string){
-	size_t pos = string.size();
+size_t pos_5th_underscore(std::string string, std::string begin){
+	std::string substr = copy_from(string, begin);
+    size_t pos = substr.size();
 	char underscore = '_';
 	int num_underscore = 0;
-	for (int i=0; i<string.size(); i++){
-		if (string[i] == underscore)
+	for (int i=0; i<substr.size(); i++){
+		if (substr[i] == underscore){
 			num_underscore++;
+        }
 		if (num_underscore == 5){
 			pos = i;
 			break;
@@ -97,13 +106,15 @@ size_t pos_5th_underscore(std::string string){
 	return pos;
 }
 
-size_t pos_4th_underscore(std::string string){
-	size_t pos = string.size();
+size_t pos_4th_underscore(std::string string, std::string begin){
+	std::string substr = copy_from(string, begin);
+    size_t pos = substr.size();
 	char underscore = '_';
 	int num_underscore = 0;
-	for (int i=0; i<string.size(); i++){
-		if (string[i] == underscore)
+	for (int i=0; i<substr.size(); i++){
+		if (substr[i] == underscore){
 			num_underscore++;
+        }
 		if (num_underscore == 4){
 			pos = i;
 			break;
@@ -111,6 +122,24 @@ size_t pos_4th_underscore(std::string string){
 	}
 	return pos;
 }
+
+size_t pos_6th_underscore(std::string string, std::string begin){
+	std::string substr = copy_from(string, begin);
+    size_t pos = substr.size();
+	char underscore = '_';
+	int num_underscore = 0;
+	for (int i=0; i<substr.size(); i++){
+		if (substr[i] == underscore){
+			num_underscore++;
+        }
+		if (num_underscore == 6){
+			pos = i;
+			break;
+		}
+	}
+	return pos;
+}
+
 
 // number of decimals (max is 6)
 int num_decimals(float num){
@@ -152,14 +181,14 @@ std::string make_mom_par(std::array<int, 4> mom){
     return mom_par;
 }
 
-// make 3momentum parameter in the form "x x x"
-std::string make_3mom_par(std::array<int, 3> mom){
-    std::string mom_par = std::to_string(mom[0]);
-    for (int d=1; d<mom.size(); d++ ){
-        mom_par += " " + std::to_string(mom[d]);
-    }
-    return mom_par;
-}
+// // make 3momentum parameter in the form "x x x"
+// std::string make_3mom_par(std::array<int, 3> mom){
+//     std::string mom_par = std::to_string(mom[0]);
+//     for (int d=1; d<mom.size(); d++ ){
+//         mom_par += " " + std::to_string(mom[d]);
+//     }
+//     return mom_par;
+// }
 
 // make momentum string in the form "x_x_x_x"
 std::string make_mom_name(std::array<int, 4> mom){
@@ -167,24 +196,24 @@ std::string make_mom_name(std::array<int, 4> mom){
     return mom_str;
 }
 
-// make 3momentum string in the form "x_x_x" 
-std::string make_3mom_name(std::array<int, 3> mom){
-    std::string mom_str = space_to_underscore(make_3mom_par(mom));
-    return mom_str;
-}
+// // make 3momentum string in the form "x_x_x" 
+// std::string make_3mom_name(std::array<int, 3> mom){
+//     std::string mom_str = space_to_underscore(make_3mom_par(mom));
+//     return mom_str;
+// }
 
-// get momentum in a string with the "mom_" keyword
+// get momentum in a string with the "snkmom_" keyword
 std::string get_snkmom(std::string string){
-    std::string mom_full = copy_from(string, "mom_");
-	std::string mom_string = copy_from(mom_full, "mom_", pos_4th_underscore(mom_full));
-	std::string mom = remove_str(mom_string, "mom_");
+    std::string mom_full = copy_from(string, "snkmom_");
+	std::string mom_string = copy_from(mom_full, "snkmom_", pos_5th_underscore(mom_full, "snkmom_"));
+	std::string mom = remove_str(mom_string, "snkmom_");
 	return mom;
 }
 
 // get momentum in a string with the "mom_" keyword
 std::string get_mom(std::string string){
     std::string mom_full = copy_from(string, "mom_");
-	std::string mom_string = copy_from(mom_full, "mom_", pos_5th_underscore(mom_full));
+	std::string mom_string = copy_from(mom_full, "mom_", pos_5th_underscore(mom_full, "mom_"));
 	std::string mom = remove_str(mom_string, "mom_");
 	return mom;
 }
@@ -222,14 +251,14 @@ std::string make_twist_name(std::array<float, 4> twist){
 // get twist in a string with the "tw_" keyword
 std::string get_twist(std::string string){
     std::string tw_string_full = copy_from(string, "tw_");
-	std::string tw_string = copy_from(tw_string_full, "tw_", pos_5th_underscore(tw_string_full));
+	std::string tw_string = copy_from(tw_string_full, "tw_", pos_5th_underscore(tw_string_full, "tw_"));
 	std::string twist = remove_str(tw_string, "tw_");
 	return twist;
 }
 
 // remove "_tw_" string and the value of momentum in the form "x_x_x_x"
 std::string remove_twist_str(std::string string){ 
-    std::string toErase = copy_from(string, "_tw_", pos_5th_underscore(string));
+    std::string toErase = copy_from(string, "_tw_", pos_6th_underscore(string, "_tw_"));
     size_t pos = string.find(toErase);
     if (pos != std::string::npos){   
         string.erase(pos, toErase.length());
