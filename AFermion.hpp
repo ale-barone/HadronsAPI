14bf82df245548e0baac185d98d8fb0cb4110ce1
Alessandro Barone <ab1n19@soton.ac.uk>
2022-07-04 17:51:54 +0100

    Add make_propgator_name

--- AFermion.hpp
+++ AFermion.hpp
@@ -1,7 +1,6 @@
 #include "Utilities.hpp"
 
-BEGIN_API_NAMESPACE
-BEGIN_APIMODULE_NAMESPACE("Fermion")
+BEGIN_APIMODULE_NAMESPACE(AFermion)
 
 // assign the MFermion::GaugeProp parameters
 template <typename TFermion>
@@ -10,45 +9,45 @@ void assign_propagator_par(TFermion &quark, std::string source, std::string solv
     quark.solver = solver;
 }
 
-// // make the name for the propagator
-// template <typename TFermioin>
-// std::string make_propagator_name(TFermion quark, std::string incipit){
 
-// }
+
+std::string make_propagator_name(std::string name, std::string source, std::string solver){
+    
+    std::string twist = get_twist(solver);
+    std::string twist_name = "";
+    if (!twist.empty() && !(twist=="0_0_0_0" || twist=="0.0_0.0_0.0_0.0"))
+        twist_name = "_tw_" + twist;
+
+    std::string mom = get_mom(source);
+    std::string mom_name = "";
+    if (!mom.empty() && !(twist=="0_0_0_0"))
+        mom_name = "_mom_" + mom;
+    
+    std::string quark_source = "";
+    if (has_string(source, "source_seq"))
+        quark_source = "_" + remove_twist_str(remove_str(remove_str(source, "source_seq_"), "sm_"));
+    
+    std::string name_prop = 
+          "quark_" + name 
+        + quark_source
+        + twist_name;// + mom_name;
+    return name_prop;
+}
 
 // PROPAGATOR
 std::string make_propagator(Application &application, std::string name, std::string source, std::string solver){
     MFermion::GaugeProp::Par quark;
-
     quark.source = source;
     quark.solver = solver;
 
-    std::string twist = get_twist(solver);
-    std::string name_prop;
-    if (twist.empty())
-        name_prop = "quark_" + name;
-    else
-        name_prop = "quark_" + name + "_" + twist;
+    std::string name_prop = make_propagator_name(name, source, solver);
     application.createModule<MFermion::GaugeProp>(name_prop, quark);
     return name_prop;
 }
 
 // SEQUENTIAL PROPAGATOR
 std::string make_seq_propagator(Application &application, std::string name, std::string seq_source, std::string solver){
-    MFermion::GaugeProp::Par quark;
-
-    quark.source = seq_source;
-    quark.solver = solver;
-
-    std::string twist;
-    if (get_twist(solver).empty())
-        twist = "";
-    else
-        twist = "_" + get_twist(solver);
-    
-    std::string name_prop = "quark_" + name + "_" + remove_twist_str(remove_str(remove_str(seq_source, "source_seq_"), "sm_")) + twist;
-    application.createModule<MFermion::GaugeProp>(name_prop, quark);
-    return name_prop;
+    return make_propagator(Application &application, std::string name, std::string source, std::string solver)
 }
 
 
