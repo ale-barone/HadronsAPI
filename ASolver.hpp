c846df98ad8197f20091f757702ca98f55d0f99b
Alessandro Barone <ab1n19@soton.ac.uk>
2022-08-20 14:41:40 +0100

    Add option for custom name

--- ASolver.hpp
+++ ASolver.hpp
@@ -3,7 +3,20 @@
 
 BEGIN_APIMODULE_NAMESPACE(ASolver)
 
+// custum names
+std::string make_solver_CG(Application &application, std::string solver_name, std::string action, int maxIter, double res){
+    MSolver::RBPrecCG::Par solver;
+    solver.action       = action;
+    solver.maxIteration = maxIter;
+    solver.residual     = res;
 
+    std::string quark_name = remove_str(action, "action_");
+    solver_name += "_" + quark_name;
+    application.createModule<MSolver::RBPrecCG>(solver_name, solver);
+    return solver_name;
+}
+
+// automatic naming
 std::string make_solver_CG(Application &application, std::string action, int maxIter, double res){
     MSolver::RBPrecCG::Par solver;
     solver.action       = action;
@@ -15,5 +28,7 @@ std::string make_solver_CG(Application &application, std::string action, int max
     application.createModule<MSolver::RBPrecCG>(solver_name, solver);
     return solver_name;
 }
+
+
 
 END_APIMODULE_NAMESPACE
